d163558ea0811dbbebc7559c757a12cb5b0adc45
Akpeti Trust <akpetitrust@gmail.com>
2022-01-06 16:07:20 +0100

    Font issues fixed

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -5,7 +5,7 @@ import stanrute from "./Stanrute.png";
 export default function Preloader() {
   return (
     <section aria-hidden className={style.preloader}>
-      <img src={stanrute} alt="Logo" />
+      <img src={stanrute} alt="Starute Logo" />
     </section>
   );
 }
