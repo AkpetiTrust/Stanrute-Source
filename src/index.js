97b268bb28f29bfffb48cf31eb47a26be4a8d8c2
Akpeti Trust <akpetitrust@gmail.com>
2022-03-07 21:30:16 +0100

    Finishing touches

--- src/index.js
+++ src/index.js
@@ -11,22 +11,21 @@ import Blog from "./pages/Blog/Blog";
 import ScrollToTop from "./ScrollToTop";
 
 import AOS from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 AOS.init({
   once: true,
-})
-
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <ScrollToTop />
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/academy' exact element={<Academy />} />
-        <Route path='/careers' exact element={<Career />} />
-        <Route path='/about' exact element={<About />} />
-        <Route path='/blog' exact element={<Blog />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/academy" exact element={<Academy />} />
+        <Route path="/careers" exact element={<Career />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/blog" exact element={<Blog />} />
       </Routes>
     </Router>
   </React.StrictMode>,
